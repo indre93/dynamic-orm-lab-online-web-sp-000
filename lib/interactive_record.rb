cf09eaa38b851cf8bdd827591049999fbbe0f29a
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-09 01:53:18 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -25,7 +25,7 @@ class InteractiveRecord
   end
 
   def self.create(define_attributes)
-    student = self.class.new(name, grade)
+    student = self.class.new(define_attributes)
     student.save
   end
 
