2db6a8c8b0f98cd3dc378667661150dd0f063ba2
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-09 02:01:44 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,7 +21,7 @@ class InteractiveRecord
     attributes_hash.each do |keys, value|
       self.send("#{keys}=", value)
     end
-    Student.create(attributes_hash)
+    self.class.create(attributes_hash)
   end
 
   def self.create(define_attributes)
